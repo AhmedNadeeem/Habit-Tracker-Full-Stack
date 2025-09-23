31e11f70b4728dce2222f12615cfcfe0a2701ecf
Ahmed Nadeem Gondal <ag1858085@gmail.com>
2025-09-24 04:28:10 +0500

    Cors added in backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,34 @@
-const express = require("express")
-const habitRouter = require("./Routes/habit.routes")
-const userRouter = require("./Routes/user.routes")
-const mongoose = require('mongoose');
-const dotenv = require("dotenv")
+const express = require("express");
+const habitRouter = require("./Routes/habit.routes");
+const userRouter = require("./Routes/user.routes");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-dotenv.config()
+dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL)
-.then (() => console.log("Connected to mongoDB!"))
-.catch((err) => console.error(`Failed to connect to mongoDB. Error details: ${err}`));
+const frontendUrl = process.env.FRONTEND_URL;
 
-const app = express()
+const corsOptions = {
+  origin: frontendUrl,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to mongoDB!"))
+  .catch((err) =>
+    console.error(`Failed to connect to mongoDB. Error details: ${err}`)
+  );
 
-app.use("/api/v1/auth/", userRouter)
-app.use("/api/v1/habits/", habitRouter)
+const app = express();
 
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(8000, console.log("App is listening to port 8000."))
+app.use("/api/v1/auth/", userRouter);
+app.use("/api/v1/habits/", habitRouter);
+
+app.listen(8000, console.log("App is listening to port 8000."));
