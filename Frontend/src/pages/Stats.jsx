3a56af1add0f2c9d2da21029ed8df51ccbc752a3
Ahmed Nadeem Gondal <ag1858085@gmail.com>
2025-10-12 03:03:33 +0500

    Stats component improvement

--- Frontend/src/pages/Stats.jsx
+++ Frontend/src/pages/Stats.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import IdeaIcon from "../assets/idea.png"
 import WorkIcon from "../assets/work.png"
 import StudyIcon from "../assets/study.png"
@@ -10,7 +10,8 @@ import HomeIcon from "../assets/home.png"
 import MeditationIcon from "../assets/meditation.png"
 import SocialIcon from "../assets/social.png"
 import SportsIcon from "../assets/sports.png"
-import HabitDialog from '../components/HabitDialog';
+import axios from 'axios'
+import { useSelector } from 'react-redux'
 
 const icons = {
   idea: IdeaIcon,
@@ -26,7 +27,7 @@ const icons = {
   sports: SportsIcon
 }
 
-const dates = [
+const datesArr = [
   {date: 8, day: "Mon", status: false},
   {date: 9, day: "Tue", status: true},
   {date: 10, day: "Wed", status: true},
@@ -37,7 +38,47 @@ const dates = [
 ]
 
 export default function Stats() {
-  const [date,setDate] = React.useState(Date.now())
+  const curDate = new Date();
+  const [weekDates,setWeekDates] = React.useState([]);
+  const [weekProgress, setWeekProgress] = React.useState([]);
+  const [startDate, setStartDate] = React.useState(curDate);
+
+  const userData = useSelector((state) => state.user.user);
+  const userId = userData.user.userId;
+
+  const getWeekDates = (inputDate)=>{
+    const baseDate = new Date(inputDate);
+    const tempWeekDates = [];
+
+    for (let i = -2; i <= 4; i++){
+      const tempDate = new Date(baseDate);
+      tempDate.setDate(baseDate.getDate() + i);
+      tempWeekDates.push(tempDate);
+    }
+
+    console.log(tempWeekDates);
+    setWeekDates(tempWeekDates);
+  }
+
+  const nextWeekDates = ()=>{
+    let tempDate = new Date(startDate);
+    tempDate.setDate(startDate.getDate() + 7);
+    console.log(tempDate);
+    setStartDate(tempDate);
+  };
+  
+  const prevWeekDates = ()=>{
+    let tempDate = new Date(startDate);
+    tempDate.setDate(startDate.getDate() - 7);
+    console.log(tempDate);
+    setStartDate(tempDate);
+  }
+
+
+  useEffect(()=>{
+    getWeekDates(startDate);
+
+  }, [setWeekDates, startDate])
   return (
     <div className='min-h-screen flex flex-col bg-black p-10'>
         <div className='bg-gray-600 p-6 px-10 rounded-2xl flex flex-col justify-between'>
@@ -49,19 +90,19 @@ export default function Stats() {
           </div>
 
         <div className='px-6 flex justify-between items-center mt-6'>
-            <button className='cursor-pointer bg-[#ff9100] h-10 w-10 rounded-4xl hover:bg-[#dd7e00] text-black text-2xl font-bold'>
+            <button onClick={prevWeekDates} className='cursor-pointer bg-[#ff9100] h-10 w-10 rounded-4xl hover:bg-[#dd7e00] text-black text-2xl font-bold'>
               {"<"}
             </button>
             <div className='flex gap-16'>
-              {dates.map(date => (
+              {weekDates && weekDates?.map(date => (
 
-                <div className={`${date.status ? "bg-[#ff9100]" : "bg-gray-400"} w-20 h-25 flex flex-col justify-center items-center rounded-lg`}>
-                  <p className='text-2xl font-bold text-black'>{date.date}</p>
-                  <p className='text-lg'>{date.day}</p>
+                <div className={`${false ? "bg-[#ff9100]" : "bg-gray-400"} w-20 h-25 flex flex-col justify-center items-center rounded-lg`}>
+                  <p className='text-2xl font-bold text-black'>{date.getDate()}</p>
+                  <p className='text-lg'>{date.getDay() == 0 ? "Sun" : date.getDay() == 1 ? "Mon" : date.getDay() == 2 ? "Tue" : date.getDay() == 3 ? "Wed" : date.getDay() == 4 ? "Thu" : date.getDay() == 5 ? "Fri" : "Sat"}</p>
                 </div>
 ))}
             </div>
-            <button className='cursor-pointer bg-[#ff9100] h-10 w-10 rounded-4xl hover:bg-[#dd7e00] text-black text-2xl font-bold'>
+            <button onClick={nextWeekDates} className='cursor-pointer bg-[#ff9100] h-10 w-10 rounded-4xl hover:bg-[#dd7e00] text-black text-2xl font-bold'>
               {">"}
             </button>
         </div>
